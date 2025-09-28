30fcdf6b329529bc726b60cf8ce84d459de72b9d
Tarekul_Islam <tarekulislam0422@gmail.com>
2025-09-28 12:36:47 +0600

    Template test

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
 
-Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
-Route::get('/courses/create', [CourseController::class, 'create'])->name('courses.create');
+Route::get('/', [CourseController::class, 'index'])->name('courses.index');
+Route::get('/create', [CourseController::class, 'create'])->name('courses.create');
 Route::post('/courses', [CourseController::class, 'store'])->name('courses.store');
-Route::delete('/courses/{course}', [CourseController::class, 'destroy'])->name('courses.destroy');
+Route::delete('/{course}', [CourseController::class, 'destroy'])->name('courses.destroy');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::view('/student', 'frontend.master')->name('frontend.master');
+;
 
